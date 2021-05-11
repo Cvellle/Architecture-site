ffabdf2e536868ebb6d0b4dbc44299dd509e756a
cvellle <cuekisa@yahoo.com>
2021-05-11 21:31:19 +0200

    build new

---
+++ client/build/precache-manifest.a42886ea85e41b72d90007c2d5a618ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "cde73f14951d3849f92e",
+    "url": "/static/js/main.cde73f14.chunk.js"
+  },
+  {
+    "revision": "caf0330ad1cdf8b5a055",
+    "url": "/static/js/2.caf0330a.chunk.js"
+  },
+  {
+    "revision": "cde73f14951d3849f92e",
+    "url": "/static/css/main.ed7aa2a8.chunk.css"
+  },
+  {
+    "revision": "caf0330ad1cdf8b5a055",
+    "url": "/static/css/2.7bd3bcd5.chunk.css"
+  },
+  {
+    "revision": "3c24c26762110426884af46751f08739",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.295bd70a053d1a4cfdca5718ed6498d2.js"
+  "/precache-manifest.a42886ea85e41b72d90007c2d5a618ee.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
