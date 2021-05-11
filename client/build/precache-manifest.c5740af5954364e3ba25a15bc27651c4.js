a1029aa56805e7b15c6f87e7d24e8b5d05c8c516
cvellle <cuekisa@yahoo.com>
2021-05-11 20:20:06 +0200

    server

---
+++ client/build/precache-manifest.c5740af5954364e3ba25a15bc27651c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb3136ea9ce35395c98d",
+    "url": "./static/js/runtime~main.cb3136ea.js"
+  },
+  {
+    "revision": "e560501c1fe86650a1c5",
+    "url": "./static/js/main.e560501c.chunk.js"
+  },
+  {
+    "revision": "41ec6aeb6ec5f7e97ae0",
+    "url": "./static/js/2.41ec6aeb.chunk.js"
+  },
+  {
+    "revision": "e560501c1fe86650a1c5",
+    "url": "./static/css/main.451c5b60.chunk.css"
+  },
+  {
+    "revision": "41ec6aeb6ec5f7e97ae0",
+    "url": "./static/css/2.7bd3bcd5.chunk.css"
+  },
+  {
+    "revision": "e9a27acd9d15cf73fa46839982d6570a",
+    "url": "./index.html"
+  }
+];
--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const cors = require("cors");
+const path = require("path");
+
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(cookieParser());
 
-const { sendEmail } = require('./mail');
+const { sendEmail } = require("./mail");
 
 app.post("/api/sendEmail", (req, res) => {
-    sendEmail(req);
-})
+  sendEmail(req);
+});
 
-app.listen(PORT,  () => {
-    console.log( "Server Running at " + PORT);
-})
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Server Running at " + PORT);
+});
