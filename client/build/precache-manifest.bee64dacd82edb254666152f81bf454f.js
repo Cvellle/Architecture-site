4505b486e52b11aba6b8dd3a13c7997d2afc10dd
cvellle <cuekisa@yahoo.com>
2021-05-13 12:11:16 +0200

    check 1

---
+++ client/build/precache-manifest.bee64dacd82edb254666152f81bf454f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "6bc8928db2196f622cf3",
+    "url": "/static/js/main.6bc8928d.chunk.js"
+  },
+  {
+    "revision": "f3f855af847fa8ebe679",
+    "url": "/static/js/2.f3f855af.chunk.js"
+  },
+  {
+    "revision": "6bc8928db2196f622cf3",
+    "url": "/static/css/main.2118b472.chunk.css"
+  },
+  {
+    "revision": "f3f855af847fa8ebe679",
+    "url": "/static/css/2.04095def.chunk.css"
+  },
+  {
+    "revision": "9f3d6d484941cce8e1f1f79d68e01714",
+    "url": "/index.html"
+  }
+];
