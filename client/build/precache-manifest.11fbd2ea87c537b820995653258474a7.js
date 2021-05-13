a0bdae8e968e26a9a2a1186b0ed076112a1f64df
cvellle <cuekisa@yahoo.com>
2021-05-13 13:36:01 +0200

    check 1

---
+++ client/build/precache-manifest.11fbd2ea87c537b820995653258474a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "604105e249174b5d65a7",
+    "url": "/static/js/main.604105e2.chunk.js"
+  },
+  {
+    "revision": "f77943178edfe7fb4677",
+    "url": "/static/js/2.f7794317.chunk.js"
+  },
+  {
+    "revision": "604105e249174b5d65a7",
+    "url": "/static/css/main.a8161d53.chunk.css"
+  },
+  {
+    "revision": "f77943178edfe7fb4677",
+    "url": "/static/css/2.04095def.chunk.css"
+  },
+  {
+    "revision": "d07e952e43c67794df5f1b9eceaac748",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e09f4295f330ad93c503b053cae12b0e.js"
+  "/precache-manifest.11fbd2ea87c537b820995653258474a7.js"
 );
 
 workbox.clientsClaim();
