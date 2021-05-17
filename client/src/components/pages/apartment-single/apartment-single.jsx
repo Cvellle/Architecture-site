b780e72e5419e5fb81f8ff370a620e19cc7f28ee
cvellle <cuekisa@yahoo.com>
2021-05-17 22:38:30 +0200

    4

--- client/src/components/pages/apartment-single/apartment-single.jsx
+++ client/src/components/pages/apartment-single/apartment-single.jsx
@@ -13,7 +13,7 @@ class ApartmentSinglePage extends Component {
     infinite: true,
     speed: 1000,
     slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToScroll: 1
   };
 
   render() {
@@ -25,7 +25,9 @@ class ApartmentSinglePage extends Component {
           parent={[{ path: "apartments", title: "Stanovi" }]}
         />
         <Helmet>
-          <title>Gening | {item.title}</title>
+          <title>
+            Gening | {item.title}
+          </title>
         </Helmet>
         <ContentWrap isBoxed={true}>
           <div className="entry-image text-center">
@@ -41,90 +43,67 @@ class ApartmentSinglePage extends Component {
                       </tr>
                     </thead>
                     <tr>
-                      <td className="different">br.</td>
-                      <td className="different">PROSTORIJA</td>
-                      <td className="different">
+                      <td>br.</td>
+                      <td>PROSTORIJA</td>
+                      <td>
                         P m<sup>2</sup>
                       </td>
-                      <td className="different">
-                        O m<sup>1</sup>
-                      </td>
-                      <td className="different">VRSTA PODA</td>
+                      <td>VRSTA PODA</td>
                     </tr>
-                    {item.rooms.map((content, index) => (
+                    {item.rooms.map((content, index) =>
                       <tr>
-                        <td>0{index + 1}</td>
-                        <td>{content.room}</td>
-                        <td>{content.povrsina}</td>
-                        <td>{content.obim}</td>
-                        <td>{content.pod}</td>
+                        <td>
+                          0{index + 1}
+                        </td>
+                        <td>
+                          {content.room}
+                        </td>
+                        <td>
+                          {content.povrsina}
+                        </td>
+                        <td>
+                          {content.pod}
+                        </td>
                       </tr>
-                    ))}
+                    )}
                     <tr>
-                      <td colspan="6" className="different">
-                        &nbsp;
-                      </td>
+                      <td colspan="5">&nbsp;</td>
                     </tr>
                     <tr>
                       <td colspan="2" className="text-left pl-2">
                         Neto zatvorena površina
                       </td>
-                      <td>{item.netoPovrsina.zatvorena}</td>
                       <td>
-                        {item.netoPovrsina.umanjenjeZ < 100
-                          ? `-${item.netoPovrsina.umanjenjeZ}%`
-                          : null}
+                        {item.netoPovrsina.zatvorena}
                       </td>
-                      <td>
-                        {item.netoPovrsina.umanjenjeZ > 0
-                          ? (
-                              item.netoPovrsina.zatvorena *
-                              ((100 - item.netoPovrsina.umanjenjeZ) / 100)
-                            ).toFixed(2)
-                          : item.netoPovrsina.zatvorena}
-                      </td>
+                      <td />
                     </tr>
                     <tr>
                       <td colspan="2" className="text-left pl-2">
                         Neto otvorena površina
                       </td>
-                      <td>{item.netoPovrsina.otvorena}</td>
                       <td>
-                        {item.netoPovrsina.umanjenjeO > 0
-                          ? `${item.netoPovrsina.umanjenjeO}`
-                          : null}
+                        {item.netoPovrsina.otvorena}
                       </td>
-                      <td>
-                        {item.netoPovrsina.umanjenjeO > 0
-                          ? (
-                              item.netoPovrsina.otvorena *
-                              ((100 - item.netoPovrsina.umanjenjeO) / 100)
-                            ).toFixed(2)
-                          : item.netoPovrsina.otvorena}
-                      </td>
+                      <td />
                     </tr>
                     <tr>
                       <td colspan="2" className="text-left pl-2">
                         Ukupna neto površina
                       </td>
-                      <td />
-                      <td />
                       <td>
-                        {(
-                          (item.netoPovrsina.umanjenjeZ < 100
-                            ? item.netoPovrsina.zatvorena -
-                              item.netoPovrsina.zatvorena *
-                                (item.netoPovrsina.umanjenjeZ / 100)
-                            : item.netoPovrsina.zatvorena) +
-                          item.netoPovrsina.otvorena
-                        ).toFixed(2)}
+                        {item.netoPovrsina.ukupna}
                       </td>
+                      <td />
                     </tr>
                   </table>
+                  <figure className="apartment__imageWrapper col-12 col-md-12 mt-5 mx-auto">
+                    <img src={item.gallery[1]} alt="" />
+                  </figure>
                 </div>
               </div>
               <div className=" col-12 col-md-6 m-5 m-md-0">
-                <figure className="apartment__imageWrapper col-12 col-md-9 m-auto">
+                <figure className="apartment__imageWrapper col-12 col-md-12 m-auto">
                   <img src={item.gallery[0]} alt="" />
                 </figure>
               </div>
@@ -139,7 +118,7 @@ class ApartmentSinglePage extends Component {
 const mapStateToProps = (state, ownProps) => {
   let apartmentID = ownProps.match.params.title;
   return {
-    item: state.apartments.apartments.find((el) => el.title == apartmentID),
+    item: state.apartments.apartments.find(el => el.title == apartmentID)
   };
 };
 
